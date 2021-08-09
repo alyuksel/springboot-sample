5a678b9d64cdaf87df58323eb6133032cd4e7f6c
ayuksel <alparslan.yuksel@capgemini.com>
2021-08-09 16:16:23 +0200

    Add initial test

---
+++ src/test/com/alpi/service/PersonServiceTest.java
@@ -0,0 +1,14 @@
+package com.alpi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+    @Test
+    void shouldSaveUser() {
+        //TODO implementer le vrai test
+        assertTrue(true);
+    }
+}
